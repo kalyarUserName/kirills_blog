0d6e126c2c79ed719246d218e78dfa1ec294986e
kalyarUserName <ron230298@email.com>
2022-08-15 19:10:31 +0300

    add redux user, utils

---
+++ src/store/store.ts
@@ -0,0 +1,53 @@
+import { compose, createStore, applyMiddleware, Middleware } from "redux";
+import logger from "redux-logger";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
+import createSagaMiddleware from "redux-saga";
+import storage from "redux-persist/lib/storage";
+
+import { rootReducer } from "./rootReducer";
+import { rootSaga } from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  sagaMiddleware,
+].filter((middleware): middleware is Middleware => Boolean(middleware));
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const persistConfig: ExtendedPersistConfig = {
+  key: "root",
+  storage,
+  whitelist: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
